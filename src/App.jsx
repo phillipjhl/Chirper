269223e447db4bf09a430ba296cf17cd8c997222
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-08-30 22:52:02 -0500

    created simple homepage of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 
 class App extends Component {
     constructor(props) {
         super(props);
     }
 
-    render () {
+    render() {
+
         return (
             <Fragment>
-                <h1>Welcome to Chirper!</h1>
+                <div className="jumbotron jumbotron-fluid text-primary m-0">
+                    <div className="container">
+                        <h1 className="display-4">Chirper</h1>
+                        <p className="lead">Welcome to Chirper. A site to post fun messages!</p>
+                    </div>
+                </div>
+
+                <div class="pos-f-t">
+                    <div class="collapse" id="navbarToggleExternalContent">
+                        <div class="bg-primary p-4">
+                            <h5 class="text-white h4">Collapsed content</h5>
+                            <span class="text-muted">Toggleable via the navbar brand.</span>
+                        </div>
+                    </div>
+                    <nav class="navbar navbar-dark bg-dark">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </nav>
+                </div>
             </Fragment>
-        )
+        );
     }
 }
 
